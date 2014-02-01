e3e720527f8c4914e3371a6a1203bad6f93f1cbc
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2014-02-01 20:09:08 +0900

    Add spork support to test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,19 +1,32 @@
-ENV["RAILS_ENV"] ||= "test"
-require File.expand_path("../../config/environment", __FILE__)
-require "rails/test_help"
-require "database_cleaner"
+helper = lambda {
+  ENV["RAILS_ENV"] ||= "test"
+  require File.expand_path("../../config/environment", __FILE__)
+  require "rails/test_help"
+  require "database_cleaner"
 
-class ActiveSupport::TestCase
-  ActiveRecord::Migration.check_pending!
+  class ActiveSupport::TestCase
+    ActiveRecord::Migration.check_pending!
 
-  DatabaseCleaner.strategy = :transaction
-  #fixtures :all
+    DatabaseCleaner.strategy = :transaction
+    #fixtures :all
 
-  def setup
-    DatabaseCleaner.start
+    def setup
+      DatabaseCleaner.start
+    end
+
+    def teardown
+      DatabaseCleaner.clean
+    end
+  end
+}
+
+unless defined?(Spork) && Spork.using_spork?
+  helper.call
+else
+  Spork.prefork do
+    helper.call
   end
 
-  def teardown
-    DatabaseCleaner.clean
+  Spork.each_run do
   end
 end
