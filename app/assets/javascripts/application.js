86b15fbf45d34a025e0fa5eb6a86facbec3f919c
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2014-02-07 14:38:53 +0900

    Add SetCursorLine plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery-autosize
-//= require set-number/dist/set-number.min.js
+//= require set-number/dist/set-number.min
+//= require set-cursor-line/dist/set-cursor-line.min
 //= require moment
 //= require turbolinks
 //= require_tree .
