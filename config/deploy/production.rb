5939c62f0a7ef3c0169e3821d1a9efc808c45934
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2014-05-04 04:44:17 +0900

    Add db role for spruce-tree.net

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server "spruce-tree.net", user: "florena", roles: %w{web app}
+server "spruce-tree.net", user: "florena", roles: %w{web app db}
 
 
 # Custom SSH Options
