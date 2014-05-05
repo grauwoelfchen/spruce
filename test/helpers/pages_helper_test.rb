3fa701b362e2fc38ad7fab8200670f951a82d2dd
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2014-05-05 18:36:03 +0900

    Update intro image link with image_path
    * Use `image_path` for production hash value
    * Add Document for screenshot method
    
    fix #1

--- test/helpers/pages_helper_test.rb
+++ test/helpers/pages_helper_test.rb
@@ -5,17 +5,17 @@ class PagesHelperTest < ActionView::TestCase
 
   def test_screenshot
     result = screenshot("test.png", "test image", "200x160")
-    assert_match /\/assets\/test\.png/, result
-    assert_match /target="_blank"/,     result
-    assert_match /alt="test\simage"/,   result
-    assert_match /height="160"/,        result
-    assert_match /width="200"/,         result
+    assert_match /href="\/images\/test\.png"/, result
+    assert_match /target="_blank"/,            result
+    assert_match /alt="test\simage"/,         result
+    assert_match /height="160"/,              result
+    assert_match /width="200"/,               result
     assert_match /src="\/images\/test\.png"/, result
   end
 
   def test_screenshot_without_size
     expected = <<-HTML.gsub(/\n|^\s*/, "")
-<a href="/assets/test.png" target="_blank">
+<a href="/images/test.png" target="_blank">
   <img alt="test image" height="160" src="/images/test.png" width="200" />
 </a>
     HTML
@@ -24,7 +24,7 @@ class PagesHelperTest < ActionView::TestCase
 
   def test_screenshot_without_alt
     expected = <<-HTML.gsub(/\n|^\s*/, "")
-<a href="/assets/test.png" target="_blank">
+<a href="/images/test.png" target="_blank">
   <img height="80" src="/images/test.png" width="100" />
 </a>
     HTML
