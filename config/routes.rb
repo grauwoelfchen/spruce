5d8eaef9c9724c0fbd0d6d4b59372c1c362dc43d
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2014-12-20 14:50:53 +0900

    Allow .txt format for Note#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,44 +1,46 @@
 Spruce::Application.routes.draw do
   # authentication
-  delete "logout" => "sessions#destroy", :as => :logout
-  get    "login"  => "sessions#new",     :as => :login
-  post   "login"  => "sessions#create"
+  delete :logout, to: "sessions#destroy", as: :logout
+  get    :login,  to: "sessions#new",     as: :login
+  post   :login,  to: "sessions#create"
 
   # signup & activation
-  get  "signup" => "users#new", :as => :signup
-  post "signup" => "users#create"
-  get "users/:token/activate", :to => "users#activate", :as => :activate,
-    :constraints => {:token => /[A-z0-9]+/}
+  get  :signup, to: "users#new", as: :signup
+  post :signup, to: "users#create"
+  get "users/:token/activate", to: "users#activate", as: :activate,
+    constraints: {token: /[A-z0-9]+/}
 
   # reset password
   resources :password_resets,
-    :param  => :token,
-    :except => [:index, :show, :destroy]
+    param:  :token,
+    except: [:index, :show, :destroy]
 
-  resources :nodes, :path => "b", :shallow => true, :only => [:index] do
-    options = {:except => [:index, :destroy], :with => [:delete]}
-    resources :nodes, {:path => "b"}.merge(options)
-    resources :notes, {:path => "l"}.merge(options)
+  resources :nodes, path: "b", shallow: true, only: [:index] do
+    options = {
+      except: [:index, :destroy],
+      with: [:delete]
+    }
+    resources :nodes, {path: "b"}.merge(options)
+    resources :notes, {path: "l"}.merge(options) do
+      get :show, constraints: {format: %w(html txt)}
+    end
   end
 
   # versions
-  get  "v/:id/:type/revert", :to => "versions#revert", :as => :revert_version,
-    :constraints => RevertConstraint.new
-  post "v/:id/:type/revert", :to => "versions#restore",
-    :constraints => RevertConstraint.new
+  get  "v/id/type/revert", to: "versions#revert", as: :revert_version,
+    constraints: RevertConstraint.new
+  post "v/id/type/revert", to: "versions#restore",
+    constraints: RevertConstraint.new
 
   # pages
-  get "introduction", :to => "pages#introduction"
-  get "changelog",    :to => "pages#changelog"
-  get "syntax",       :to => "pages#syntax"
+  get :introduction, to: "pages#introduction"
+  get :changelog,    to: "pages#changelog"
+  get :syntax,       to: "pages#syntax"
 
-  get "sitemap", :to => "sitemap#index", :constraints => {:format => "xml"}
+  get :sitemap, to: "sitemap#index", constraints: {format: "xml"}
 
   # errors
-  match ":code",
-    :to          => "errors#show",
-    :constraints => {:status => /\d{3}/ },
-    :via         => :all
+  match ":code", to: "errors#show", via: :all, constraints: {code: /\d{3}/ }
 
   root "pages#index"
 end
