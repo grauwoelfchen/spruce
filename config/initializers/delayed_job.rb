19770c9bbb1070e14442ae2a61bdb386b340268a
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2015-01-07 17:46:31 +0900

    Improve delayed_job config

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,8 +1,8 @@
 Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.sleep_delay = 15
-Delayed::Worker.max_attempts = 2
+Delayed::Worker.sleep_delay = 30
+Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 3.minutes
-Delayed::Worker.read_ahead = 10
+Delayed::Worker.read_ahead = 5
 Delayed::Worker.default_queue_name = "default"
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.raise_signal_exceptions = :term
