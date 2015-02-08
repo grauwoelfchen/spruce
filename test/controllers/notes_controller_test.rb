527a64a77b844fcee8d05b22ab5f91ffe51316a3
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2015-02-09 05:24:05 +0900

    Fix Nodes, Notes and Versions tests. ref #36

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -1,51 +1,52 @@
 require "test_helper"
 
 class NotesControllerTest < ActionController::TestCase
-  include RequestHelper
+  fixtures(:notes, :users, :nodes)
 
-  fixtures :notes, :users, :nodes
-
-  setup :login, :build_node_tree, :initialize_note
-  teardown :logout
+  setup(:login, :build_node_tree, :initialize_note)
+  teardown(:logout)
 
   # actions
 
   def test_get_index_without_nest
-    assert_raise ActionController::UrlGenerationError do
-      get :index
+    assert_raise(ActionController::UrlGenerationError) do
+      get(:index)
     end
   end
 
   def test_get_index_with_others_node
-    assert_raise ActionController::UrlGenerationError do
-      bob_s_node = nodes(:bob_s_home)
-      get :index, :node_id => bob_s_node.id
+    bob_s_node = nodes(:bob_s_home)
+    assert_raise(ActionController::UrlGenerationError) do
+      get(:index, :node_id => bob_s_node.id)
     end
   end
 
   def test_get_index
-    assert_raise ActionController::UrlGenerationError do
-      get :index, :node_id => @note.node.id
+    assert_raise(ActionController::UrlGenerationError) do
+      get(:index, :node_id => @note.node.id)
     end
   end
 
   def test_get_show_with_others_note
     bob_s_note = notes(:idea_note)
-    assert_raise ActiveRecord::RecordNotFound do
-      get :show, :id => bob_s_note.id
+    assert_raise(ActiveRecord::RecordNotFound) do
+      get(:show, :id => bob_s_note.id)
     end
+    refute(assigns(:note))
+    refute(assigns(:node))
   end
 
   def test_get_show
-    get :show, :id => @note.id
-    assert_response :success
-    assert_equal @note, assigns(:note)
-    assert_template :show
+    get(:show, :id => @note.id)
+    assert_equal(@note, assigns(:note))
+    assert_equal(@note.node, assigns(:node))
+    assert_template(:show)
+    assert_response(:success)
   end
 
   def test_get_new_without_nest
-    assert_raise ActionController::UrlGenerationError do
-      get :new
+    assert_raise(ActionController::UrlGenerationError) do
+      get(:new)
     end
   end
 
@@ -54,20 +55,21 @@ class NotesControllerTest < ActionController::TestCase
     assert_raise ActiveRecord::RecordNotFound do
       get :new, :node_id => bob_s_node.id
     end
+    refute(assigns(:node))
   end
 
   def test_get_new
-    get :new, :node_id => @note.node.id
-    assert_response :success
-    assert_instance_of Note, assigns(:note)
-    assert_template :new
-    assert_template :partial => "_form"
+    get(:new, :node_id => @note.node.id)
+    assert_instance_of(Note, assigns(:note))
+    assert_equal(@note.node, assigns(:node))
+    assert_template(:new)
+    assert_template(:partial => "_form")
+    assert_response(:success)
   end
 
   def test_post_create_without_nest
-    assert_raise ActionController::UrlGenerationError do
-      post :create,
-        :note => {:content => "* Unknown Note\r\n* Unexpected"}
+    assert_raise(ActionController::UrlGenerationError) do
+      post(:create, :note => {:content => "* Unknown Note\r\n* Unexpected"})
     end
   end
 
@@ -79,11 +81,13 @@ class NotesControllerTest < ActionController::TestCase
         :content => "Not allowed, right?\r\n"
       }
     }
-    assert_no_difference "Node.count", 1 do
-      assert_raise ActiveRecord::RecordNotFound do
-        post :create, params
+    assert_no_difference("Node.count", 1) do
+      assert_raise(ActiveRecord::RecordNotFound) do
+        post(:create, params)
       end
     end
+    refute(assigns(:node))
+    refute(assigns(:note))
   end
 
   def test_post_create_with_validation_errors
@@ -93,14 +97,17 @@ class NotesControllerTest < ActionController::TestCase
         :content => ""
       }
     }
-    assert_no_difference "Note.count", 1 do
-      post :create, params
+    assert_no_difference("Note.count", 1) do
+      post(:create, params)
     end
-    assert_response :success
-    assert_instance_of Note, assigns(:note)
-    assert_template :new
-    assert_template :partial => "shared/_errors"
-    assert_template :partial => "_form"
+    assert_instance_of(Note, assigns(:note))
+    refute(assigns(:note).persisted?)
+    assert_equal(@note.node, assigns(:node))
+    assert_nil(flash[:notice])
+    assert_template(:new)
+    assert_template(:partial => "shared/_errors")
+    assert_template(:partial => "_form")
+    assert_response(:success)
   end
 
   def test_post_create
@@ -110,29 +117,36 @@ class NotesControllerTest < ActionController::TestCase
         :content => "* More Hard Linux beginner's Book\r\n"
       }
     }
-    assert_difference "Note.count", 1 do
-      post :create, params
+    assert_difference("Note.count", 1) do
+      post(:create, params)
     end
-    assert_response :redirect
-    assert_redirected_to note_url(assigns(:note))
-    assert_equal \
+    assert_instance_of(Note, assigns(:note))
+    assert(assigns(:note).persisted?)
+    assert_equal(@note.node, assigns(:node))
+    assert_equal(
       "Successfully created leaf. undo",
       ActionController::Base.helpers.strip_tags(flash[:notice])
+    )
+    assert_response(:redirect)
+    assert_redirected_to(note_url(assigns(:note)))
   end
 
   def test_get_edit_with_others_note
     bob_s_note = notes(:idea_note)
-    assert_raise ActiveRecord::RecordNotFound do
-      get :edit, :id => bob_s_note.id
+    assert_raise(ActiveRecord::RecordNotFound) do
+      get(:edit, :id => bob_s_note.id)
     end
+    refute(assigns(:note))
+    refute(assigns(:node))
   end
 
   def test_get_edit
-    get :edit, :id => @note.id
-    assert_response :success
-    assert_equal @note, assigns(:note)
-    assert_template :edit
-    assert_template :partial => "_form"
+    get(:edit, :id => @note.id)
+    assert_equal(@note, assigns(:note))
+    assert_equal(@note.node, assigns(:node))
+    assert_template(:edit)
+    assert_template(:partial => "_form")
+    assert_response(:success)
   end
 
   def test_put_update_with_others_note
@@ -143,18 +157,23 @@ class NotesControllerTest < ActionController::TestCase
         :content => "Not allowed, right?"
       }
     }
-    assert_raise ActiveRecord::RecordNotFound do
-      put :update, params
+    assert_raise(ActiveRecord::RecordNotFound) do
+      put(:update, params)
     end
+    refute(flash[:notice])
+    refute(assigns(:note))
+    refute(assigns(:node))
   end
 
   def test_put_update_with_validation_errors
-    put :update, :id => @note.id, :note => {:content => ""}
-    assert_response :success
-    assert_equal @note, assigns(:note)
-    assert_template :edit
-    assert_template :partial => "shared/_errors"
-    assert_template :partial => "_form"
+    put(:update, :id => @note.id, :note => {:content => ""})
+    assert_response(:success)
+    assert_equal(@note, assigns(:note))
+    assert_equal(@note.node, assigns(:node))
+    refute(flash[:notice])
+    assert_template(:edit)
+    assert_template(:partial => "shared/_errors")
+    assert_template(:partial => "_form")
   end
 
   def test_put_update
@@ -165,63 +184,82 @@ class NotesControllerTest < ActionController::TestCase
           "* Little Hard Linux user's Book\r\n* Getting Started"
       }
     }
-    put :update, params
-    assert_response :redirect
-    assert_redirected_to note_url(assigns(:note))
-    assert_equal \
+    put(:update, params)
+    assert_equal(params[:note][:content], assigns(:note).content)
+    assert_equal(@note.node, assigns(:node))
+    assert_equal(
       "Successfully updated leaf. undo",
       ActionController::Base.helpers.strip_tags(flash[:notice])
+    )
+    assert_response(:redirect)
+    assert_redirected_to(note_url(assigns(:note)))
   end
 
   def test_get_delete_with_others_note
     bob_s_note = notes(:idea_note)
-    assert_raise ActiveRecord::RecordNotFound do
-      get :delete, :id => bob_s_note.id
+    assert_raise(ActiveRecord::RecordNotFound) do
+      get(:delete, :id => bob_s_note.id)
     end
+    refute(assigns(:note))
+    refute(assigns(:node))
   end
 
   def test_get_delete
-    get :delete, :id => @note.id
-    assert_response :success
-    assert_equal @note, assigns(:note)
-    assert_template :delete
+    get(:delete, :id => @note.id)
+    assert_equal(@note, assigns(:note))
+    assert_equal(@note.node, assigns(:node))
+    assert_template(:delete)
+    assert_response(:success)
   end
 
   def test_delete_destroy_with_others_note
     bob_s_note = notes(:idea_note)
-    assert_no_difference "Note.count", -1 do
-      assert_raise ActiveRecord::RecordNotFound do
-        delete :destroy, :id => bob_s_note.id
+    assert_no_difference("Note.count", -1) do
+      assert_raise (ActiveRecord::RecordNotFound) do
+        delete(:destroy, :id => bob_s_note.id)
       end
     end
+    refute(assigns(:note))
+    refute(assigns(:node))
   end
 
   def test_delete_destroy
-    assert_difference "Note.count", -1 do
-      delete :destroy, :id => @note.id
+    assert_difference("Note.count", -1) do
+      delete(:destroy, :id => @note.id)
     end
-    assert_response :redirect
-    assert_redirected_to node_url(@note.node)
-    assert_equal \
+    assert_equal(@note, assigns(:note))
+    refute(assigns(:note).persisted?)
+    assert_equal(
       "Successfully destroyed leaf. undo",
       ActionController::Base.helpers.strip_tags(flash[:notice])
+    )
+    assert_response(:redirect)
+    assert_redirected_to(node_url(@note.node))
   end
 
   # methods
 
+  def test_load_note
+    controller = NotesController.new
+    controller.request = request
+    controller.params[:id] = @note.id
+
+    controller.send(:load_note)
+    assert_equal(@note, controller.instance_variable_get(:@note))
+  end
+
   def test_undo_link
+    controller = NotesController.new
+    controller.request = request
     @note.update_attribute(:name, "Updated hard Linux Book")
     prev_version = @note.versions.last
-    controller = NotesController.new
     controller.instance_variable_set(:@note, @note)
 
-    with_request(controller) do
-      expected = <<-LINK.gsub(/^\s{8}|\n/, "")
-        <a data-method="post"
-         href="/v/#{prev_version.id}/l/revert" rel="nofollow">undo</a>
-      LINK
-      assert_equal expected, controller.send(:undo_link)
-    end
+    expected = <<-LINK.gsub(/^\s{6}|\n/, "")
+      <a data-method="post"
+       href="/v/#{prev_version.id}/l/revert" rel="nofollow">undo</a>
+    LINK
+    assert_equal(expected, controller.send(:undo_link))
   end
 
   private
--- test/controllers/versions_controller_test.rb
+++ test/controllers/versions_controller_test.rb
@@ -1,12 +1,10 @@
 require 'test_helper'
 
 class VersionsControllerTest < ActionController::TestCase
-  include RequestHelper
+  fixtures(:nodes, :notes, :users)
 
-  fixtures :nodes, :notes, :users
-
-  setup :login, :build_node_tree
-  teardown :logout
+  setup(:login, :build_node_tree)
+  teardown(:logout)
 
   # actions
 
@@ -17,9 +15,10 @@ class VersionsControllerTest < ActionController::TestCase
       :id   => "0",
       :type => "b"
     }
-    get :revert, params
-    assert_response :redirect
-    assert_redirected_to root_url
+    get(:revert, params)
+    refute(assigns(:version))
+    assert_response(:redirect)
+    assert_redirected_to(root_url)
   end
 
   def test_get_revert_note_with_invalid_version
@@ -27,35 +26,36 @@ class VersionsControllerTest < ActionController::TestCase
       :id   => "0",
       :type => "l"
     }
-    get :revert, params
-    assert_response :redirect
-    assert_redirected_to root_url
+    get(:revert, params)
+    refute(assigns(:version))
+    assert_response(:redirect)
+    assert_redirected_to(root_url)
   end
 
   def test_get_revert_node_with_valid_version
     node = nodes(:var)
     node.update_attributes(:name => "var v2")
-    request.env["HTTP_REFERER"] = nodes_url
     params = {
       :id   => node.versions.last.id,
       :type => "b"
     }
-    get :revert, params
-    assert_response :success
-    assert_template :revert
+    get(:revert, params)
+    assert_equal(node.versions.last, assigns(:version))
+    assert_template(:revert)
+    assert_response(:success)
   end
 
   def test_get_revert_note_with_valid_version
     note = notes(:wish_list)
     note.update_attributes(:content => "* New Wishlist\r\n")
-    request.env["HTTP_REFERER"] = note_url(note)
     params = {
       :id   => note.versions.last.id,
       :type => "l"
     }
-    get :revert, params
-    assert_response :success
-    assert_template :revert
+    get(:revert, params)
+    assert_equal(note.versions.last, assigns(:version))
+    assert_response(:success)
+    assert_template(:revert)
   end
 
   # restore
@@ -66,9 +66,10 @@ class VersionsControllerTest < ActionController::TestCase
       :type => "b"
     }
     post :restore, params
-
-    assert_response :redirect
-    assert_redirected_to root_url
+    refute(assigns(:version))
+    refute(flash[:notice])
+    assert_response(:redirect)
+    assert_redirected_to(root_url)
   end
 
   def test_post_restore_note_with_invalid_version
@@ -76,35 +77,39 @@ class VersionsControllerTest < ActionController::TestCase
       :id   => "0",
       :type => "l"
     }
-    post :restore, params
-    assert_response :redirect
-    assert_redirected_to root_url
+    post(:restore, params)
+    refute(assigns(:version))
+    refute(flash[:notice])
+    assert_response(:redirect)
+    assert_redirected_to(root_url)
   end
 
   def test_post_restore_node_with_others_version
     bob_s_node = nodes(:work)
     bob_s_node.update_attributes(:name => "Bob's Home v2")
-    request.env["HTTP_REFERER"] = nodes_url
     params = {
       :id   => bob_s_node.versions.last.id,
       :type => "b"
     }
-    post :restore, params
-    assert_response :redirect
-    assert_redirected_to root_url
+    post(:restore, params)
+    refute(assigns(:version))
+    refute(flash[:notice])
+    assert_response(:redirect)
+    assert_redirected_to(root_url)
   end
 
   def test_post_restore_note_with_others_version
     bob_s_note = notes(:shopping_list)
     bob_s_note.update_attributes(:content => "* Shopping list v2\r\n")
-    request.env["HTTP_REFERER"] = note_url(bob_s_note)
     params = {
       :id   => bob_s_note.versions.last.id,
       :type => "l"
     }
-    post :restore, params
-    assert_response :redirect
-    assert_redirected_to root_url
+    post(:restore, params)
+    refute(assigns(:version))
+    refute(flash[:notice])
+    assert_response(:redirect)
+    assert_redirected_to(root_url)
   end
 
   # restore create
@@ -116,71 +121,91 @@ class VersionsControllerTest < ActionController::TestCase
   def test_post_restore_update_node_on_undo
     node = nodes(:var)
     node.update_attributes(:name => "var v2")
-    request.env["HTTP_REFERER"] = nodes_url
     params = {
       :id   => node.versions.last.id,
       :type => "b",
       :redo => "false"
     }
-    post :restore, params
-    assert_response :redirect
-    assert_redirected_to nodes_url
-    assert_equal \
+    request.env["HTTP_REFERER"] = nodes_url
+
+    assert_difference("Version::Cycle.count", 1) do
+      post(:restore, params)
+    end
+    assert_equal(node.versions.first, assigns(:version))
+    assert_equal(
       "Undid update. redo",
       ActionController::Base.helpers.strip_tags(flash[:notice])
+    )
+    assert_response(:redirect)
+    assert_redirected_to(nodes_url)
   end
 
   def test_post_restore_update_note_on_undo
     note = notes(:wish_list)
-    request.env["HTTP_REFERER"] = note_url(note)
     note.update_attributes(:name => "* My Wishlist v2")
     params = {
       :id   => note.versions.last.id,
       :type => "l",
       :redo => "false"
     }
-    post :restore, params
-    assert_response :redirect
-    assert_redirected_to note_url(note)
-    assert_equal \
+    request.env["HTTP_REFERER"] = note_url(note)
+
+    assert_difference("Version::Cycle.count", 1) do
+      post(:restore, params)
+    end
+    assert_equal(note.versions.first, assigns(:version))
+    assert_equal(
       "Undid update. redo",
       ActionController::Base.helpers.strip_tags(flash[:notice])
+    )
+    assert_response(:redirect)
+    assert_redirected_to(note_url(note))
   end
 
   def test_post_restore_update_node_on_redo
     node = nodes(:var)
     node.update_attributes(:name => "var v2")
     node.versions.last.restore!
-    request.env["HTTP_REFERER"] = nodes_url
     params = {
       :id   => node.versions.last.id,
       :type => "b",
       :redo => "true",
     }
-    post :restore, params
-    assert_response :redirect
-    assert_redirected_to nodes_url
-    assert_equal \
+    request.env["HTTP_REFERER"] = nodes_url
+
+    assert_difference("Version::Cycle.count", 1) do
+      post(:restore, params)
+    end
+    assert_equal(node.versions[1], assigns(:version))
+    assert_equal(
       "Undid update. undo",
       ActionController::Base.helpers.strip_tags(flash[:notice])
+    )
+    assert_response(:redirect)
+    assert_redirected_to(nodes_url)
   end
 
   def test_post_restore_update_note_on_redo
     note = notes(:wish_list)
     note.update_attributes(:name => "var v2")
     note.versions.last.restore!
-    request.env["HTTP_REFERER"] = note_url(note)
     params = {
       :id   => note.versions.last.id,
       :type => "l",
       :redo => "true",
     }
-    post :restore, params
-    assert_response :redirect
-    assert_redirected_to note_url(note)
-    assert_equal \
+    request.env["HTTP_REFERER"] = note_url(note)
+
+    assert_difference("Version::Cycle.count", 1) do
+      post(:restore, params)
+    end
+    assert_equal(note.versions[1], assigns(:version))
+    assert_equal(
       "Undid update. undo",
       ActionController::Base.helpers.strip_tags(flash[:notice])
+    )
+    assert_response(:redirect)
+    assert_redirected_to(note_url(note))
   end
 
   # restore destroy
@@ -198,12 +223,13 @@ class VersionsControllerTest < ActionController::TestCase
     controller = VersionsController.new
     controller.instance_variable_set(:@version, version)
 
-    with_params(controller, :type => "b", :redo => "false") do
+    request.stub(:parameters, {:type => "b", :redo => "false"}) do |request|
+      controller.request = request
       expected = <<-LINK.gsub(/^\s{8}|\n/, "")
         <a data-method="post" href="/v/#{version.next.id}/b/revert?redo=true"
          rel="nofollow">redo</a>
       LINK
-      assert_equal expected, controller.send(:redo_link)
+      assert_equal(expected, controller.send(:redo_link))
     end
   end
 
@@ -216,12 +242,13 @@ class VersionsControllerTest < ActionController::TestCase
     controller = VersionsController.new
     controller.instance_variable_set(:@version, version)
 
-    with_params(controller, :type => "b", :redo => "true") do
+    request.stub(:parameters, {:type => "b", :redo => "true"}) do |request|
+      controller.request = request
       expected = <<-LINK.gsub(/^\s{8}|\n/, "")
         <a data-method="post" href="/v/#{version.next.id}/b/revert?redo=false"
          rel="nofollow">undo</a>
       LINK
-      assert_equal expected, controller.send(:redo_link)
+      assert_equal(expected, controller.send(:redo_link))
     end
   end
 
@@ -239,8 +266,9 @@ class VersionsControllerTest < ActionController::TestCase
     controller = VersionsController.new
     controller.instance_variable_set(:@version, version)
 
-    with_referer(controller, node_url(node)) do
-      assert_equal :back, controller.send(:back_url)
+    request.stub(:referer, node_url(node)) do |request|
+      controller.request = request
+      assert_equal(:back, controller.send(:back_url))
     end
   end
 
@@ -258,12 +286,33 @@ class VersionsControllerTest < ActionController::TestCase
     controller = VersionsController.new
     controller.instance_variable_set(:@version, version)
 
-    with_referer(controller, nil) do
-      assert_equal nodes_url, controller.send(:back_url)
+    request.stub(:referer, nil) do |request|
+      controller.request = request
+      assert_equal(nodes_url, controller.send(:back_url))
     end
   end
 
-  def test_back_url_for_new_note
+  def test_back_url_for_new_note_with_referer
+    attributes = {
+      :content => "* New note\r\n",
+      :user    => users(:tim),
+    }
+    node = nodes(:tim_s_home)
+    note = node.notes.new(attributes)
+    note.save
+    version = note.versions.last
+    version.restore!
+
+    controller = VersionsController.new
+    controller.instance_variable_set(:@version, version)
+
+    request.stub(:referer, note_url(node.notes.first)) do |request|
+      controller.request = request
+      assert_equal(node_url(version.item.node), controller.send(:back_url))
+    end
+  end
+
+  def test_back_url_for_new_note_without_referer
     attributes = {
       :content => "* New note\r\n",
       :user    => users(:tim),
@@ -276,8 +325,9 @@ class VersionsControllerTest < ActionController::TestCase
     controller = VersionsController.new
     controller.instance_variable_set(:@version, version)
 
-    with_request(controller) do
-      assert_equal node_url(version.item.node), controller.send(:back_url)
+    request.stub(:referer, nil) do |request|
+      controller.request = request
+      assert_equal(node_url(version.item.node), controller.send(:back_url))
     end
   end
 
@@ -290,8 +340,9 @@ class VersionsControllerTest < ActionController::TestCase
     controller = VersionsController.new
     controller.instance_variable_set(:@version, version)
 
-    with_referer(controller, node_url(node)) do
-      assert_equal :back, controller.send(:back_url)
+    request.stub(:referer, node_url(node)) do |request|
+      controller.request = request
+      assert_equal(:back, controller.send(:back_url))
     end
   end
 
@@ -304,8 +355,9 @@ class VersionsControllerTest < ActionController::TestCase
     controller = VersionsController.new
     controller.instance_variable_set(:@version, version)
 
-    with_referer(controller, note_url(note)) do
-      assert_equal :back, controller.send(:back_url)
+    request.stub(:referer, note_url(note)) do |request|
+      controller.request = request
+      assert_equal(:back, controller.send(:back_url))
     end
   end
 
@@ -318,8 +370,9 @@ class VersionsControllerTest < ActionController::TestCase
     controller = VersionsController.new
     controller.instance_variable_set(:@version, version)
 
-    with_referer(controller, nil) do
-      assert_equal nodes_url, controller.send(:back_url)
+    request.stub(:referer, nil) do |request|
+      controller.request = request
+      assert_equal(nodes_url, controller.send(:back_url))
     end
   end
 
@@ -332,7 +385,8 @@ class VersionsControllerTest < ActionController::TestCase
     controller = VersionsController.new
     controller.instance_variable_set(:@version, version)
 
-    with_referer(controller, nil) do
+    request.stub(:referer, nil) do |request|
+      controller.request = request
       assert_equal nodes_url, controller.send(:back_url)
     end
   end
