4bd3d496066697d51c4e426844a18d5714f7654d
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2015-02-13 02:49:57 +0900

    Fix helper test. ref #49

--- test/helpers/pages_helper_test.rb
+++ test/helpers/pages_helper_test.rb
@@ -5,12 +5,13 @@ class PagesHelperTest < ActionView::TestCase
 
   def test_screenshot
     result = screenshot("test.png", "test image", "200x160")
-    assert_match /href="\/images\/test\.png"/, result
-    assert_match /target="_blank"/,            result
-    assert_match /alt="test\simage"/,         result
-    assert_match /height="160"/,              result
-    assert_match /width="200"/,               result
-    assert_match /src="\/images\/test\.png"/, result
+
+    assert_match(/href="\/images\/test\.png"/, result)
+    assert_match(/target="_blank"/, result)
+    assert_match(/alt="test\simage"/, result)
+    assert_match(/height="160"/, result)
+    assert_match(/width="200"/, result)
+    assert_match(/src="\/images\/test\.png"/, result)
   end
 
   def test_screenshot_without_size
@@ -19,7 +20,8 @@ class PagesHelperTest < ActionView::TestCase
   <img alt="test image" height="160" src="/images/test.png" width="200" />
 </a>
     HTML
-    assert_equal expected, screenshot("test.png", "test image")
+
+    assert_equal(expected, screenshot("test.png", "test image"))
   end
 
   def test_screenshot_without_alt
@@ -28,6 +30,7 @@ class PagesHelperTest < ActionView::TestCase
   <img height="80" src="/images/test.png" width="100" />
 </a>
     HTML
-    assert_equal expected, screenshot("test.png", nil, "100x80")
+
+    assert_equal(expected, screenshot("test.png", nil, "100x80"))
   end
 end
