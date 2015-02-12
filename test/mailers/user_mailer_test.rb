9111b00fe8c5ff1a0dbffbfb41458ed11978c058
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2015-02-13 02:52:01 +0900

    Fix mailers. ref #49

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,34 +1,37 @@
 require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
-  fixtures :users
+  fixtures(:users)
 
-  setup :initialize_user
+  setup(:initialize_user)
 
   def test_reset_password_email
     @user.reset_password_token = "token"
     mail = UserMailer.reset_password_email(@user)
-    assert_equal "Your password has been reset", mail.subject
-    assert_equal [@user.email], mail.to
-    assert_equal ["from@example.org"], mail.from
-    assert_match "Hi, #{@user.username}", mail.body.encoded
+
+    assert_equal("Your password has been reset", mail.subject)
+    assert_equal([@user.email], mail.to)
+    assert_equal(["from@example.org"], mail.from)
+    assert_match("Hi, #{@user.username}", mail.body.encoded)
   end
 
   def test_activation_needed_email
     @user.activation_token = "token"
     mail = UserMailer.activation_needed_email(@user)
-    assert_equal "Welcome to Spruce", mail.subject
-    assert_equal [@user.email], mail.to
-    assert_equal ["from@example.org"], mail.from
-    assert_match "Welcome to Spruce, #{@user.username}", mail.body.encoded
+
+    assert_equal("Welcome to Spruce", mail.subject)
+    assert_equal([@user.email], mail.to)
+    assert_equal(["from@example.org"], mail.from)
+    assert_match("Welcome to Spruce, #{@user.username}", mail.body.encoded)
   end
 
   def test_activation_success_email
     mail = UserMailer.activation_success_email(@user)
-    assert_equal "Your account is now activated", mail.subject
-    assert_equal [@user.email], mail.to
-    assert_equal ["from@example.org"], mail.from
-    assert_match "Congratulations, #{@user.username}", mail.body.encoded
+
+    assert_equal("Your account is now activated", mail.subject)
+    assert_equal([@user.email], mail.to)
+    assert_equal(["from@example.org"], mail.from)
+    assert_match("Congratulations, #{@user.username}", mail.body.encoded)
   end
 
   private
